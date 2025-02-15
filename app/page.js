a3ff94482dbbeeb97e5045ac9877bea2c56e0f8a
Pedro Leite <pedrolte11@hotmail.com>
2025-02-14 22:05:51 -0300

    APRENDEMOS MUITA COISA
    Não sei explicar, mas aprendi muita coisa. Aprendi a trazer os produtos de sites direto

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // get busca
 // post coloca 
@@ -13,30 +13,61 @@ export default function Home() {
   const [pokemons, alteraPokemons] = useState([])
   const [pesquisa, alteraPesquisa] = useState("")
 
+  const [erroPesquisa, alteraErroPesquisa] = useState(false)
+
   async function busca(){
-     const response = await axios.get ("https://pokeapi.co/api/v2/pokemon/lugia")
-     const data = response.data;
-     console.log(data);
+
+    try{
+      const response = await axios.get ("https://pokeapi.co/api/v2/pokemon/"+pesquisa)
+ 
+         const data = response.data;
+         alteraPokemons([data])
+         alteraErroPesquisa(false)
+
+    }catch(e){
+      alteraErroPesquisa(true)
+    }
   }
+
+  async function buscaTodos(){
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon/")
+    console.log(response);
+    const data = response.data.results;
+    alteraPokemons(data)
+  }
+
+  useEffect( ()=> {
+    buscaTodos();
+  }, [] )
 
   // function buscaTodos(){ } para o projeto
   
-
 
   return (
     <div>
       <h1> Pokedex</h1>
 
       <p> Digite o nome de um pokemon</p>
-      <input></input>
+      <input onChange={(e)=> alteraPesquisa(e.target.value) }></input>
       <br/>
       <button onClick={()=>busca()}>Pesquisar</button>
+      {erroPesquisa == true && <p className="text-red-500" > Caracteres inválidos </p>}
 
       <hr/>
 
-      <h2>Nome</h2>
-      <p><strong>ID:</strong>??</p>
-      <img src=""/>
+      {
+        pokemons == 0 ?
+          <p>Carregando pokedex</p>
+          :
+        pokemons.map( (i, index) =>
+          <div className="flex gap-10 flex-wrap">
+            <h2>{i.name}</h2> 
+            <p><strong>ID:</strong>{ i.id ? i.id : index+1}</p>
+            <img src= {"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/shiny/"+(  i.id ? i.id : index+1)+".gif"}/>
+          </div>
+        ) 
+      }
+
     </div>
   );
 }
